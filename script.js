a94323d569c1b38280be2c799270f74160fe1589
Saydeezr <saydeezr@gmail.com>
2024-09-12 15:15:48 -0400

    took out onClick and made js dynamic navigation plus some refactoring

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
 const start = document.getElementById('play-btn');
-const next = document.getElementById('next');
+const nextButton = document.querySelectorAll('.next-btn');
+const backButton = document.querySelectorAll('.back-btn');
 let formData = {};
 
 //pulls up form and hides landing page section
-const formPopUp = async (event) => {
+const formPopUp = (event) => {
     event.preventDefault();
 
-    console.log('I have been clicked! Lets start the game!');
+    console.log('Lets start the game!');
     document.getElementById('section1').classList.add('hidden');
     document.getElementById('section2').classList.remove('hidden');
 };
@@ -16,43 +17,66 @@ const formPopUp = async (event) => {
 function checkForm(currentGroup) {
     const formGroup = document.getElementById(currentGroup);
     const inputs = formGroup.querySelectorAll('input');
-    let allFilled = true;
+    let isValid = true;
 
        // loops over all inputs
         for (let i = 0; i < inputs.length; i++) {
             inputs[i].required = true;
-            console.log(inputs[i].value);
+            // console.log(inputs[i].value);
         // if input left empty, notifies user
             if (!inputs[i].value) {
                 console.log('INPUT REQUIRED');
-                allFilled = false;
+                isValid = false;
             } else {
                 formData[inputs[i].id] =inputs[i].value
-                console.log(formData)
+                
             }
         }
-        return allFilled;
+        console.log(formData)
+        return isValid;
 };
 
 
 //navigate between forms/form groups
-function showForm(formId) {
+function showForm(event) {
+    if (event) {
+        event.preventDefault();
+    }
+
+    const button = event.target;
+    const targetGroupId = button.getAttribute('data-target');
+    console.log('Navigating to:', targetGroupId);
+
     const currentGroup = document.querySelector('.form-group:not(.hidden)');
     const currentGroupId = currentGroup.id;
 
+    //validate current form
     if (checkForm(currentGroupId)) {
+        // Hide all forms and show the target form
     const forms = document.querySelectorAll('.form-group');
     forms.forEach(form => form.classList.add('hidden'));
     document.getElementById(formId).classList.remove('hidden');
-
     }
   };
 
 
   //function to get calculate results
   const submitResults = () => {
-    console.log('Submitted Results!')
+    console.log('Submitted Results!');
+    console.log(formData);
   };
 
 
-start.addEventListener("click", formPopUp);        
+start.addEventListener("click", formPopUp); 
+
+nextButton.forEach(button => {
+    button.addEventListener('click', (event) => {
+        showForm(event);
+    });
+});
+
+backButton.forEach(button => {
+    button.addEventListener('click', (event) => {
+        showForm(event);
+    });
+});
